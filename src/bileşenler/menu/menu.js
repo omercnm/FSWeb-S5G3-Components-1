aeffa99d134d88b0312007b643e13b9e7e8fc016
ömer <omercnm23@gmail.com>
2023-01-29 20:17:56 +0300

    S5G3 tamamlandı.

--- src/bileşenler/menu/menu.js
+++ src/bileşenler/menu/menu.js
@@ -1,13 +1,13 @@
-import './Menu.less'
+import "./Menu.less";
 
 // Kullanacağımız veri bu, üzerinde çalışın ama henüz hiçbir şeyi değiştirmeyin.
 let menuElemanlari = [
-  'Gündem',
-  'Dünya',
+  "Gündem",
+  "Dünya",
   "Ekonomi",
-  'Yazarlar',
-  'Burç Yorumları',
-  'Diğer'
+  "Yazarlar",
+  "Burç Yorumları",
+  "Diğer",
 ];
 
 /*
@@ -34,3 +34,27 @@ let menuElemanlari = [
 
   Adım 6: 'menuYapici' fonksiyonunu ve 'menuElemanlari' dizisini kullanarak menüyü oluşturun, ve döndürülen menüyü header'e ekleyin.
 */
+
+let menuDon = document.querySelector(".header");
+const menuYapici = (mD) => {
+  const menu = document.createElement("div");
+  menu.classList.add("menu");
+
+  const list = document.createElement("ul");
+
+  for (let i = 0; i < mD.length; i++) {
+    const liA = document.createElement("li");
+    liA.textContent = mD[i];
+    list.appendChild(liA);
+  }
+  menu.appendChild(list);
+  return menu;
+};
+
+menuDon.appendChild(menuYapici(menuElemanlari));
+
+const menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", (event) => {
+  const menu = document.querySelector("div.menu");
+  menu.classList.toggle("menu--open");
+});
